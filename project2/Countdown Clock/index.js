d6358d0921d51833ebea7aa74bbaea7a2e6b14fd
Jimmy Palafox <85587136+hazedCadet@users.noreply.github.com>
2023-07-06 14:03:02 -0400

    starting timer workshop

---
+++ project2/Countdown Clock/index.js
@@ -0,0 +1,11 @@
+// script for countdown clock website
+
+
+const countdownText = document.querySelector("timer-text");
+
+function countdown() {
+    countdownText.textContent = "timer activated"
+};
+
+const countdownButton = document.getElementByClassName("button-start-timer")
+countdownButton.addEventListener("click", function countdown() {});
